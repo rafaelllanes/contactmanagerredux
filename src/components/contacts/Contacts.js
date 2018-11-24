5e3b4d96e605264806f4d538bd22ef5981897982
rafaelllanes <ralph.llanes@gmail.com>
2018-11-23 17:06:18 -0800

    redux set-up

---
+++ src/components/contacts/Contacts.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import Contact from "./Contact";
+
+class Contacts extends Component {
+  render() {
+    const { contacts } = this.state;
+    return (
+      <React.Fragment>
+        <h1 className="display-4 mb-2">
+          <span className="text-danger">Contact</span> List
+        </h1>
+        {contacts.map(contact => (
+          <Contact key={contact.id} contact={contact} />
+        ))}
+      </React.Fragment>
+    );
+  }
+}
+
+export default Contacts;
