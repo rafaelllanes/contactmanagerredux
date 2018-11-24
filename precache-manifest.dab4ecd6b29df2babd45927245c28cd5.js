88ca802579ee69012f04a58be160a380df658896
rafaelllanes <ralph.llanes@gmail.com>
2018-11-23 22:43:01 -0800

    Updates

---
+++ precache-manifest.dab4ecd6b29df2babd45927245c28cd5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "23cb3a9dd489de075243",
+    "url": "/contactmanagerredux/static/js/runtime~main.23cb3a9d.js"
+  },
+  {
+    "revision": "1490d786f74311978057",
+    "url": "/contactmanagerredux/static/js/main.1490d786.chunk.js"
+  },
+  {
+    "revision": "852838d1174690c44fe0",
+    "url": "/contactmanagerredux/static/js/1.852838d1.chunk.js"
+  },
+  {
+    "revision": "1490d786f74311978057",
+    "url": "/contactmanagerredux/static/css/main.2b97c4ad.chunk.css"
+  },
+  {
+    "revision": "852838d1174690c44fe0",
+    "url": "/contactmanagerredux/static/css/1.d2a3662c.chunk.css"
+  },
+  {
+    "revision": "798a376bf7d266c834257bcacc8e04a8",
+    "url": "/contactmanagerredux/index.html"
+  }
+];
